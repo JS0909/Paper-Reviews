1649d92c19497ec76318299189203f13937c2b92
JS0909 <zejace@naver.com>
2022-08-13 09:35:25 +0900

    commit

---
+++ m361_dacon_antena.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
+from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import r2_score, accuracy_score
+from sklearn.feature_selection import SelectFromModel
+from sklearn.pipeline import make_pipeline
+import matplotlib.pyplot as plt
+import math
+import time
+
